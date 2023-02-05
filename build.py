9b086e3501cb03f4dc0aef1db050e075f41f121a
Santiago Mola <santiago.mola@datadoghq.com>
2023-02-05 19:53:32 +0100

    Push all variants

--- build.py
+++ build.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
 import re
+import subprocess
+import sys
 
 
-VARIANTS={
+VARIANTS = {
     "8",
     "11",
     "17",
@@ -19,6 +21,7 @@ VARIANTS={
     "GRAALVM17",
 }
 
+
 def render(tpl, variant):
     res = tpl
     if variant:
@@ -26,17 +29,52 @@ def render(tpl, variant):
         variant_lower = variant.lower()
         res = res.replace("<variant_name_upper/>", variant_upper)
         res = res.replace("<variant_name_lower/>", variant_lower)
-        res = re.sub(r'^# <base>.*?^# </base>', '', res, flags=re.DOTALL|re.MULTILINE)
+        res = re.sub(r"^# <base>.*?^# </base>", "", res, flags=re.DOTALL | re.MULTILINE)
     else:
-        res = re.sub(r'^# <variant>.*?^# </variant>', '', res, flags=re.DOTALL|re.MULTILINE)
+        res = re.sub(
+            r"^# <variant>.*?^# </variant>", "", res, flags=re.DOTALL | re.MULTILINE
+        )
     return res
 
-with open('Dockerfile.tpl') as f:
+
+with open("Dockerfile.tpl") as f:
     tpl = f.read()
 
-with open('Dockerfile.base', 'w') as f:
+with open("Dockerfile.base", "w") as f:
     f.write(render(tpl, None))
 
 for variant in VARIANTS:
-    with open(f"Dockerfile.{variant}", 'w') as f:
+    with open(f"Dockerfile.{variant.lower()}", "w") as f:
         f.write(render(tpl, variant))
+
+
+def run(args, raise_on_status=True):
+    print(f"Running: {args}")
+    result = subprocess.run(args, capture_output=True)
+    print(f"Standard output: {result.stdout}")
+    print(f"Standard error: {result.stderr}")
+    if raise_on_status:
+        if result.returncode != 0:
+            raise Exception(f"Command: {args} exited with code {result.returncode}")
+    return result
+
+
+if "--push" in sys.argv:
+    print("Pushing images")
+    base_branch = run(["git", "branch", "--show-current"]).stdout.strip()
+    for variant in VARIANTS:
+        variant = variant.lower()
+        print(f"Pushing {variant}")
+        run(["git", "checkout", base_branch])
+        if (
+            run(
+                ["git", "rev-parse", "--verify", variant], raise_on_status=False
+            ).returncode
+            == 0
+        ):
+            run(["git", "branch", "-D", variant])
+        run(["git", "checkout", "-b", variant])
+        run(["ln", "-sf", f"Dockerfile.{variant}", "Dockerfile"])
+        run(["git", "commit", "-m", f"Update symlink for {variant}", "Dockerfile"])
+        run(["git", "push", "origin", f"+{variant}:{variant}"])
+    run(["git", "checkout", base_branch])
